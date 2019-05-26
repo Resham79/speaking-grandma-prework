d90b1c7b78c0ab98377241beff7a7feb0330606b
Resham Arora <resham.arora@gmail.com>
2019-05-26 23:23:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,12 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == ""
+  end
+end
   end
 end 
     
